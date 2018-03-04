38b84cc20ecf679df162d5a3dce5af2771404317
Sanjit Misra <misra.sanjit@gmail.com>
2018-03-04 22:10:48 +0530

    Auth Controller work started

--- src/public/controllers/AuthController.php
+++ src/public/controllers/AuthController.php
@@ -2,3 +2,29 @@
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+
+class AuthController
+{
+	public function __construct()
+	{}
+
+	public function authenticate($request, $response, $args)
+	{
+		$parsedBody = $request->getParsedBody();
+		$username = $parsedBody['username'] ?? false;
+		$password = $parsedBody['password'] ?? false;
+
+		// Get the password hash from the DB here
+		$passwordHash = "";
+		
+		if(password_verify($password, $passwordHash))
+		{
+			//User authenticated
+		}
+		else
+		{
+			//Send 401
+		}
+		
+	}
+}
--- src/public/controllers/UserController.php
+++ src/public/controllers/UserController.php
@@ -1,4 +1,12 @@
 <?php
-
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+
+
+/* Password hash creation logic
+$options = array(
+    					'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
+    					'cost' => 12,
+  						);
+  		$password_hash = password_hash($password, PASSWORD_BCRYPT, $options);
+*/
